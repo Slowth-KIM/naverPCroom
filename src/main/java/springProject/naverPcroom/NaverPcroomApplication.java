7a72bc947b63ece32ff62b26c0749e40de92871d
Slowth-KIM <rt.slowth@gmail.com>
2021-08-17 23:21:17 +0900

    프로젝트 설정

---
+++ src/main/java/springProject/naverPcroom/NaverPcroomApplication.java
@@ -0,0 +1,13 @@
+package springProject.naverPcroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverPcroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverPcroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springProject/naverPcroom/NaverPcroomApplicationTests.java
@@ -0,0 +1,13 @@
+package springProject.naverPcroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverPcroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
